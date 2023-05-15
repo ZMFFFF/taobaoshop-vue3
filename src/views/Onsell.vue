513b0e73e2f7b617b9c8623237faf7ab68a561c6
Zmengfei <2387649010@qq.com>
2023-05-15 18:03:16 +0800

    onsell：特惠页面完成

--- src/views/Onsell.vue
+++ src/views/Onsell.vue
@@ -1,6 +1,144 @@
 <template>
-    <h1>Onsell</h1>
+    <div class="onsell">
+        <div class="cont_box">
+            <template v-if="content.length != 0">
+                <div class="cont" v-for="(item, i) in content" :key="i">
+                    <div class="onSell_price">省{{ item.coupon_amount }}元</div>
+                    <img :src="item.pict_url" alt="" />
+                    <p style="color: #ff4040; font-size: 18px">
+                        <span style="text-decoration: line-through"
+                            >￥{{ item.zk_final_price }}</span
+                        >&nbsp;
+                        <span
+                            >券后价{{
+                                (
+                                    item.zk_final_price - item.coupon_amount
+                                ).toFixed(2)
+                            }}</span
+                        >
+                    </p>
+                    <p class="p_volume" style="font-size: 14px; color: #7f828b">
+                        {{ item.volume }} · 人已购买
+                    </p>
+                    <p
+                        style="
+                            width: 100%;
+                            height: 50px;
+                            font-weight: bold;
+                            overflow: hidden;
+                            text-overflow: ellipsis;
+                            display: -webkit-box;
+                            -webkit-box-orient: vertical;
+                            -webkit-line-clamp: 2;
+                        "
+                    >
+                        {{ item.title }}
+                    </p>
+                </div>
+            </template>
+            <div
+                v-else
+                style="
+                    width: 100%;
+                    height: 100%;
+                    display: flex;
+                    justify-content: center;
+                "
+            >
+                <img src="@/assets/img/22.gif" alt="" />
+            </div>
+        </div>
+    </div>
 </template>
 
-<style>
+<script setup>
+import axios from "axios";
+import { onMounted, ref } from "vue";
+
+var content = ref([]);
+var page = ref(1);
+var flag = ref(true);
+
+onMounted(() => {
+    getContent(page.value);
+    window.addEventListener("scroll", onScroll);
+});
+
+async function getContent(page) {
+    let url = `https://api.sunofbeaches.com/shop/onSell/${page}`;
+    let result = await axios.get(url);
+
+    for (
+        var i = 0;
+        i <
+        result.data.data.tbk_dg_optimus_material_response.result_list.map_data
+            .length;
+        i++
+    ) {
+        content.value.push(
+            result.data.data.tbk_dg_optimus_material_response.result_list
+                .map_data[i]
+        );
+        flag.value = true;
+    }
+}
+function onScroll() {
+    const scrollTop = document.documentElement.scrollTop;
+    const windowHeight = document.documentElement.clientHeight;
+    const scrollHeight = document.documentElement.scrollHeight;
+    if (scrollTop + windowHeight + 5 >= scrollHeight) {
+        // console.log("bottom")
+        if (flag.value) {
+            flag.value = false;
+            page.value++;
+            getContent(page.value);
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.cont_box {
+    display: flex;
+    // justify-content: space-between;
+    flex-wrap: wrap;
+    width: 1140px;
+    margin: 0 auto;
+    // border: 1px solid red;
+
+    .cont {
+        position: relative;
+        width: 275px;
+        height: 395px;
+        margin: 4.7px;
+        border: 1px solid #dfdfdf;
+        padding: 5px;
+        cursor: pointer;
+
+        &:hover {
+            border: 1px solid #c9302c;
+        }
+
+        .onSell_price {
+            position: absolute;
+            top: 5px;
+            right: 5px;
+            background: #c9302c;
+            color: #fff;
+            font-size: 14px;
+            text-align: center;
+            padding: 4.5px 17px;
+        }
+
+        img {
+            width: 263px;
+            height: 263px;
+        }
+
+        p {
+            line-height: 25px;
+            margin: 5px 0;
+        }
+    }
+}
 </style>
