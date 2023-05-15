39f0e23350ce17344fb2df580a2424433051583c
Zmengfei <2387649010@qq.com>
2023-05-15 19:55:27 +0800

    页脚

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="footer">
+        <img src="@/assets/img/PBWD)B~M1YLVOEWKTTV1Q81.png" alt=""></div>
+</template>
+
+<style scoped lang="scss">
+.footer {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    background-color: #c9302b;
+}
+</style>
